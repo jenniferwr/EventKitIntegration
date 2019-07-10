94a83037bbb509628d93e36936a32e47a385dc47
Jen Wright <jenniferwr@me.com>
2019-07-10 09:56:51 -0400

    Initial Commit

---
+++ EventKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventKitApp
+//
+//  Created by Jennifer Wright on 7/10/19.
+//  Copyright © 2019 Jennifer Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
